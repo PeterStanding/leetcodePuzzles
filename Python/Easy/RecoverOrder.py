d55e5349ee7d6203a59b34a4386f98dc1a1ac41e
Peter Standing <44238507+PeterStanding@users.noreply.github.com>
2026-03-16 15:51:45 +0000

    Recovering Order Problem

---
+++ Python/Easy/RecoverOrder.py
@@ -0,0 +1,6 @@
+def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
+        output = []
+        for i in order:
+            if i in friends:
+                output.append(i)
+        return output
